1a3ae8547962e676f2b773781975fff2c3cd8068
Karol <karol.junde@gmail.com>
2017-04-05 00:37:50 +0200

    Module AMI synchronised with gitlab

---
+++ webapp/backend.tf
@@ -0,0 +1,14 @@
+#--------------------------------------------------------------
+# S3 WITH DynamoDB LOCKING CONFIGURATION
+#--------------------------------------------------------------
+terraform {
+    backend "s3" {
+        bucket 		= "s3-terraform-project"
+        key 		= "terraform.tfstate"
+        encrypt		= true
+        lock_table 	= "DEV_STATE_LOCK"
+        region = "eu-west-1"
+
+    #  acl		= "bucket-owner-full-control"
+    }
+}
